94c92c064f2014bb21de9e76a1edd6117b13b31a
moh <osman.elrashid@gmail.com>
2019-08-14 07:00:54 +0400

    initial files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e041_csharp_mvc_paypal_e.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ git.init.bat
@@ -0,0 +1,5 @@
+Set HUB_PROTOCOL=ssh 
+git init 
+git add . && git commit -m "initial files" 
+hub create elrashid-flutter-examples/e041_csharp_mvc_paypal_e 
+git push --set-upstream origin master 
